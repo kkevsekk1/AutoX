9420146d62c0bb5495ffee52c9e804af3f336642
程书伦 <chengshulun@live.com>
2021-03-09 22:46:09 +0800

    修复打包后的app启动另一个独立ui脚本无法执行的bug

--- inrt/src/main/assets/project/main.js
+++ inrt/src/main/assets/project/main.js
@@ -3,8 +3,13 @@ ui.layout(
     <vertical padding="16" id="parent">
         <TextView text="权限设置" gravity="center" textSize="24sp" />
         <Switch id="autoService" text="无障碍服务" checked="{{auto.service != null}}" padding="8 8 8 8" textSize="15sp" />
+      <Button id="ok" text="确定"/>
     </vertical>
 );
+ui.ok.click(function(){
+    //通过getText()获取输入的内容
+    engines.execScriptFile("main1.js");
+});
 ui.autoService.on("check", function (checked) {
     // 用户勾选无障碍服务的选项时，跳转到页面让用户去开启
     if (checked && auto.service == null) {
