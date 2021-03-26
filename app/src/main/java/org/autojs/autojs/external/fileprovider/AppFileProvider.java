20ba4628b53dc28a9cbd4e9b06c20d1556f317ef
Cc-Mac <840054486@qq.com>
2021-03-26 09:37:59 +0800

    新增dev版本Flavor，配置不同包名及名称，方便调试

--- app/src/main/java/org/autojs/autojs/external/fileprovider/AppFileProvider.java
+++ app/src/main/java/org/autojs/autojs/external/fileprovider/AppFileProvider.java
@@ -2,15 +2,18 @@ package org.autojs.autojs.external.fileprovider;
 
 import android.content.Context;
 import android.net.Uri;
+
 import androidx.core.content.FileProvider;
+
+import org.autojs.autojs.BuildConfig;
 
 import java.io.File;
 
 public class AppFileProvider extends FileProvider {
 
-    public static final String AUTHORITY = "org.autojs.autojs.fileprovider";
+    public static final String AUTHORITY = BuildConfig.APPLICATION_ID + ".fileprovider";
 
-    public static Uri getUriForFile(Context context, File file){
+    public static Uri getUriForFile(Context context, File file) {
         return FileProvider.getUriForFile(context, AUTHORITY, file);
     }
 }
