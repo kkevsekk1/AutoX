1d2281c407d6c5d0c6402917aa51421e8f105761
aaron <chengshulun@live.com>
2022-01-16 13:19:16 +0800

    升级androidx 配置，兼容高版本手机

--- autojs/src/main/java/com/stardust/autojs/core/pref/Pref.kt
+++ autojs/src/main/java/com/stardust/autojs/core/pref/Pref.kt
@@ -1,11 +1,10 @@
 package com.stardust.autojs.core.pref
 
-import android.preference.PreferenceManager
+import androidx.preference.PreferenceManager;
 import com.stardust.app.GlobalAppContext
 
 object Pref {
     private val preferences = PreferenceManager.getDefaultSharedPreferences(GlobalAppContext.get())
-
     val isStableModeEnabled: Boolean
         get() {
             return preferences.getBoolean("key_stable_mode", false)
