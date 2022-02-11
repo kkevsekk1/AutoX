872b570a3e35b35c9ee064ac628f6e08eea2ae2d
syhyz <99457440+syhyz@users.noreply.github.com>
2022-02-11 18:16:04 +0800

    Update OcrResult.java

--- autojs/src/main/java/com/baidu/paddle/lite/demo/ocr/OcrResult.java
+++ autojs/src/main/java/com/baidu/paddle/lite/demo/ocr/OcrResult.java
@@ -3,13 +3,23 @@ package com.baidu.paddle.lite.demo.ocr;
 import android.graphics.Rect;
 import android.os.Parcelable;
 
-public class OcrResult {
+public class OcrResult implements Comparable {
     public float confidence;
     public float preprocessTime;
     public float inferenceTime;
     public String words;
     public Rect bounds;
     public RectLocation location;
+
+    @Override
+    public int compareTo(Object o) {
+        OcrResult s = (OcrResult) (o);
+        if (this.bounds.bottom == s.bounds.bottom) {
+            return this.bounds.left - s.bounds.left;
+        } else {
+            return this.bounds.bottom - s.bounds.bottom;
+        }
+    }
 
     public static class RectLocation {
         public int left;
