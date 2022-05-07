d6819a2d4b7a5ec1db28c66d5aeaa78b8ca5613c
weilizan71 <weilizan71@gmail.com>
2022-05-07 22:46:20 +0800

    添加toast提升

--- app/src/main/java/org/autojs/autojs/build/ApkBuilderPluginHelper.java
+++ app/src/main/java/org/autojs/autojs/build/ApkBuilderPluginHelper.java
@@ -1,6 +1,9 @@
 package org.autojs.autojs.build;
 
 import android.content.Context;
+import android.widget.Toast;
+
+import org.autojs.autojs.R;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,6 +19,7 @@ public class ApkBuilderPluginHelper {
             return context.getAssets().open(TEMPLATE_APK_PATH);
         } catch (IOException e) {
             e.printStackTrace();
+            Toast.makeText(context, R.string.text_template_apk_not_found, Toast.LENGTH_SHORT).show();
         }
         return null;
     }
