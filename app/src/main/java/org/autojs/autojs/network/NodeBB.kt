192b842846c8f19acdb635f7bd499dd19b276fb3
wilinz <weiizan71@gmail.com>
2022-06-28 23:51:01 +0800

    kotlin重构

--- app/src/main/java/org/autojs/autojs/network/NodeBB.kt
+++ app/src/main/java/org/autojs/autojs/network/NodeBB.kt
@@ -1,119 +1,113 @@
-package org.autojs.autojs.network;
+package org.autojs.autojs.network
 
-import android.content.Context;
-import android.util.Log;
-
-import com.google.gson.GsonBuilder;
-import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory;
-import com.jakewharton.retrofit2.adapter.rxjava2.HttpException;
-import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import org.autojs.autojs.R;
-import org.autojs.autojs.network.api.ConfigApi;
-import org.autojs.autojs.network.util.WebkitCookieManagerProxy;
-
-import java.io.IOException;
-import java.util.Collections;
-import java.util.Map;
-
-import io.reactivex.Observable;
-import okhttp3.OkHttpClient;
-import okhttp3.ResponseBody;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import android.content.Context
+import android.util.Log
+import com.google.gson.GsonBuilder
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.jakewharton.retrofit2.adapter.rxjava2.HttpException
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import io.reactivex.Observable
+import okhttp3.OkHttpClient
+import org.autojs.autojs.R
+import org.autojs.autojs.network.api.ConfigApi
+import org.autojs.autojs.network.util.WebkitCookieManagerProxy
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 
 /**
  * Created by Stardust on 2017/9/20.
  */
-
-public class NodeBB {
-
-    public static final String BASE_URL = "http://www.autoxjs.com/";
-    private static final NodeBB sInstance = new NodeBB();
-    private static final String LOG_TAG = "NodeBB";
-    private Map<String, String> mXCsrfToken;
-
-    private Retrofit mRetrofit;
-
-    NodeBB() {
-        mRetrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(new GsonBuilder()
-                        .setLenient()
-                        .create()))
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .addCallAdapterFactory(CoroutineCallAdapterFactory.create())
-                .client(new OkHttpClient.Builder()
-                        .cookieJar(new WebkitCookieManagerProxy())
-                        .build())
-                .build();
-    }
-
-    public static NodeBB getInstance() {
-        return sInstance;
-    }
-
-    public Retrofit getRetrofit() {
-        return mRetrofit;
-    }
-
-
-    public Observable<Map<String, String>> getXCsrfToken() {
-        if (mXCsrfToken != null)
-            return Observable.just(mXCsrfToken);
-        return mRetrofit.create(ConfigApi.class)
-                .getConfig()
-                .map(config -> mXCsrfToken = Collections.singletonMap("x-csrf-token", config.getCsrfToken()));
-    }
-
-    public void invalidateXCsrfToken() {
-        mXCsrfToken = null;
-    }
-
-    public static String getErrorMessage(Throwable e, Context context, String defaultMsg) {
-        if (!(e instanceof HttpException)) {
-            return defaultMsg;
+class NodeBB internal constructor() {
+    private var mXCsrfToken: Map<String, String>? = null
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(
+            GsonConverterFactory.create(
+                GsonBuilder()
+                    .setLenient()
+                    .create()
+            )
+        )
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .addCallAdapterFactory(CoroutineCallAdapterFactory.invoke())
+        .client(
+            OkHttpClient.Builder()
+                .cookieJar(WebkitCookieManagerProxy())
+                .build()
+        )
+        .build()
+    val xCsrfToken: Observable<Map<String, String>>
+        get() = if (mXCsrfToken != null) {
+            Observable.just(mXCsrfToken)
+        } else {
+            retrofit.create(ConfigApi::class.java)
+                .config
+                .map { config ->
+                    mapOf("x-csrf-token" to  config.csrfToken)
+                        .also {
+                            mXCsrfToken = it
+                        }
+                }
         }
-        HttpException httpException = (HttpException) e;
-        ResponseBody body = httpException.response().errorBody();
-        if (body == null)
-            return defaultMsg;
-        try {
-            String errorMessage = getErrorMessage(context, httpException, body.string());
-            return errorMessage == null ? defaultMsg : errorMessage;
-        } catch (IOException e1) {
-            e1.printStackTrace();
-            return defaultMsg;
+
+    fun invalidateXCsrfToken() {
+        mXCsrfToken = null
+    }
+
+    companion object {
+        const val BASE_URL = "http://www.autoxjs.com/"
+        val instance = NodeBB()
+        private const val LOG_TAG = "NodeBB"
+        fun getErrorMessage(e: Throwable?, context: Context, defaultMsg: String): String {
+            if (e !is HttpException) {
+                return defaultMsg
+            }
+            val httpException = e
+            val body = httpException.response().errorBody() ?: return defaultMsg
+            return try {
+                val errorMessage = getErrorMessage(context, httpException, body.string())
+                errorMessage ?: defaultMsg
+            } catch (e1: IOException) {
+                e1.printStackTrace()
+                defaultMsg
+            }
+        }
+
+        private fun getErrorMessage(
+            context: Context,
+            error: HttpException,
+            errorBody: String?
+        ): String? {
+            if (errorBody == null) return null
+            if (errorBody.contains("invalid-login-credentials")) {
+                return context.getString(R.string.nodebb_error_invalid_login_credentials)
+            }
+            if (errorBody.contains("change_password_error_match")) {
+                return context.getString(R.string.nodebb_error_change_password_error_match)
+            }
+            if (errorBody.contains("change_password_error_length")) {
+                return context.getString(R.string.nodebb_error_change_password_error_length)
+            }
+            if (errorBody.contains("email-taken")) {
+                return context.getString(R.string.nodebb_error_email_taken)
+            }
+            if (error.code() == 403) {
+                return context.getString(R.string.nodebb_error_forbidden)
+            }
+            Log.d(LOG_TAG, "unknown error: $errorBody", error)
+            return null
+        }
+
+        @JvmStatic
+        fun url(relativePath: String): String {
+            return BASE_URL + relativePath
+        }
+
+        @JvmStatic
+        fun getErrorMessage(error: Throwable?, context: Context, defaultMsg: Int): CharSequence {
+            return getErrorMessage(error, context, context.getString(defaultMsg))
         }
     }
 
-    private static String getErrorMessage(Context context, HttpException error, String errorBody) {
-        if (errorBody == null)
-            return null;
-        if (errorBody.contains("invalid-login-credentials")) {
-            return context.getString(R.string.nodebb_error_invalid_login_credentials);
-        }
-        if (errorBody.contains("change_password_error_match")) {
-            return context.getString(R.string.nodebb_error_change_password_error_match);
-
-        }
-        if (errorBody.contains("change_password_error_length")) {
-            return context.getString(R.string.nodebb_error_change_password_error_length);
-        }
-        if (errorBody.contains("email-taken")) {
-            return context.getString(R.string.nodebb_error_email_taken);
-        }
-        if (error.code() == 403) {
-            return context.getString(R.string.nodebb_error_forbidden);
-        }
-        Log.d(LOG_TAG, "unknown error: " + errorBody, error);
-        return null;
-    }
-
-    public static String url(String relativePath) {
-        return BASE_URL + relativePath;
-    }
-
-    public static CharSequence getErrorMessage(Throwable error, Context context, int defaultMsg) {
-        return getErrorMessage(error, context, context.getString(defaultMsg));
-    }
 }
