586971fe7c8d515fb30b3b0fbb7af691b6b6a0b8
wilinz <weiizan71@gmail.com>
2022-08-20 05:01:21 +0800

    优化ocr

--- app/src/main/assets/sample/PaddleOCR/PaddleOCR(CustomModel)/PaddleOCR(自定义模型路径).js
+++ app/src/main/assets/sample/PaddleOCR/PaddleOCR(CustomModel)/PaddleOCR(自定义模型路径).js
@@ -1,17 +1,14 @@
 let img = images.read("./0.jpg")
-let cpuThreadNum = 4
 // 新增：自定义模型路径(必须是绝对路径), files.path() 将相对路径转为绝对路径
 let myModelPath = files.path("./models");
-// 如果打包了自定义模型路径，则路径为"models/自定义的ocr模型文件夹名字"
-//let myModelPath = "models/my_ocr_models_dir";
 let start = new Date()
 // 识别图片中的文字，返回完整识别信息（兼容百度OCR格式）。
-let result = paddle.ocr(img, cpuThreadNum, myModelPath)
+let result = paddle.ocr(img, myModelPath)
 log('OCR识别耗时：' + (new Date() - start) + 'ms')
 toastLog("完整识别信息: " + JSON.stringify(result))
 start = new Date()
 // 识别图片中的文字，只返回文本识别信息（字符串列表）。当前版本可能存在文字顺序错乱的问题 建议先使用detect后自行排序
-const stringList = paddle.ocrText(img, cpuThreadNum, myModelPath)
+const stringList = paddle.ocrText(img, myModelPath)
 log('OCR纯文本识别耗时：' + (new Date() - start) + 'ms')
 toastLog("文本识别信息: " + JSON.stringify(stringList))
 
