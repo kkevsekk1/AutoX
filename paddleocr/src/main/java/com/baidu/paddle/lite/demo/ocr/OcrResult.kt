313b32f2434eb6ae758e3c9456115f52d5fea20b
wilinz <weiizan71@gmail.com>
2022-08-31 22:34:49 +0800

    优化代码

--- paddleocr/src/main/java/com/baidu/paddle/lite/demo/ocr/OcrResult.kt
+++ paddleocr/src/main/java/com/baidu/paddle/lite/demo/ocr/OcrResult.kt
@@ -5,15 +5,15 @@ import kotlin.math.abs
 
 data class OcrResult(
     @JvmField
-    val confidence: Float = 0f,
+    val confidence: Float,
     @JvmField
-    val preprocessTime: Float = 0f,
+    val preprocessTime: Float,
     @JvmField
-    val inferenceTime: Float = 0f,
+    val inferenceTime: Float,
     @JvmField
-    val text: String = "",
+    val text: String,
     @JvmField
-    val bounds: Rect? = null,
+    val bounds: Rect,
 ) : Comparable<OcrResult> {
 
     @Deprecated("use text", ReplaceWith("text"))
@@ -36,8 +36,8 @@ data class OcrResult(
     fun getBounds() = bounds
 
     override fun compareTo(other: OcrResult): Int {
-        val deviation = (bounds!!.height() / 2).coerceAtLeast(other.bounds!!.height() / 2)
-        return if (abs((bounds.top + bounds.bottom) / 2 - (other.bounds.top + other.bounds.bottom) / 2) < deviation) {
+        val deviation = (bounds.height() / 2f).coerceAtLeast(other.bounds.height() / 2f)
+        return if (abs((bounds.top + bounds.bottom) / 2f - (other.bounds.top + other.bounds.bottom) / 2f) < deviation) {
             bounds.left - other.bounds.left
         } else {
             bounds.bottom - other.bounds.bottom
