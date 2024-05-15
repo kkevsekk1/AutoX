dde40bb19292647549cae69786abd2f5f504f29f
sock_magic <startxingcheng@gmail.com>
2024-05-15 22:46:43 +0800

    refactor: 更新通知

--- inrt/src/main/java/com/stardust/auojs/inrt/App.kt
+++ inrt/src/main/java/com/stardust/auojs/inrt/App.kt
@@ -9,13 +9,15 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.net.Uri
+import android.os.Build
+import android.os.Build.VERSION
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import androidx.core.app.NotificationCompat
 import androidx.preference.PreferenceManager
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.target.CustomViewTarget
 import com.bumptech.glide.request.transition.Transition
 import com.fanjun.keeplive.KeepLive
 import com.fanjun.keeplive.config.ForegroundNotification
@@ -28,6 +30,7 @@ import com.stardust.auojs.inrt.pluginclient.AutoXKeepLiveService
 import com.stardust.autojs.core.ui.inflater.ImageLoader
 import com.stardust.autojs.core.ui.inflater.util.Drawables
 import com.stardust.autojs.execution.ScriptExecuteActivity
+import com.stardust.util.SdkVersionUtil
 import org.autojs.autoxjs.inrt.BuildConfig
 import org.autojs.autoxjs.inrt.R
 
@@ -36,7 +39,7 @@ import org.autojs.autoxjs.inrt.R
  * Created by Stardust on 2017/7/1.
  */
 
-class App : Application() {
+class App(view: ImageView) : Application() {
 
     var TAG = "inrt.application";
     override fun onCreate() {
@@ -58,13 +61,17 @@ class App : Application() {
             override fun loadIntoBackground(view: View, uri: Uri) {
                 Glide.with(this@App)
                     .load(uri)
-                    .into(object : SimpleTarget<Drawable>() {
+                    .into(object : CustomViewTarget<View, Drawable>(view) {
                         override fun onResourceReady(
                             resource: Drawable,
                             transition: Transition<in Drawable>?
                         ) {
                             view.background = resource
                         }
+
+                        override fun onLoadFailed(errorDrawable: Drawable?) = Unit
+
+                        override fun onResourceCleared(placeholder: Drawable?) = Unit
                     })
             }
 
@@ -79,13 +86,17 @@ class App : Application() {
             ) {
                 Glide.with(this@App)
                     .load(uri)
-                    .into(object : SimpleTarget<Drawable>() {
+                    .into(object : CustomViewTarget<View, Drawable>(view) {
                         override fun onResourceReady(
                             resource: Drawable,
                             transition: Transition<in Drawable>?
                         ) {
                             drawableCallback.onLoaded(resource)
                         }
+
+                        override fun onLoadFailed(errorDrawable: Drawable?) = Unit
+
+                        override fun onResourceCleared(placeholder: Drawable?) = Unit
                     })
             }
 
@@ -93,13 +104,17 @@ class App : Application() {
                 Glide.with(this@App)
                     .asBitmap()
                     .load(uri)
-                    .into(object : SimpleTarget<Bitmap>() {
+                    .into(object : CustomViewTarget<View, Bitmap>(view) {
                         override fun onResourceReady(
                             resource: Bitmap,
                             transition: Transition<in Bitmap>?
                         ) {
                             bitmapCallback.onLoaded(resource)
                         }
+
+                        override fun onLoadFailed(errorDrawable: Drawable?) = Unit
+
+                        override fun onResourceCleared(placeholder: Drawable?) = Unit
                     })
             }
         })
@@ -118,7 +133,7 @@ class App : Application() {
                 "点击打开【" + GlobalAppContext.appName + "】",
                 R.mipmap.ic_launcher
             )  //定义前台服务的通知点击事件
-            { context, intent ->
+            { context, _ ->
                 Log.d(TAG, "foregroundNotificationClick: ");
                 val splashActivityintent = Intent(context, ScriptExecuteActivity::class.java)
                 splashActivityintent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -138,24 +153,27 @@ class App : Application() {
     }
 
     private fun showNotification(context: Context) {
+        val intent = Intent(context, SplashActivity::class.java)
+        val pi =
+            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
         val manager: NotificationManager =
             context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        val builder: Notification.Builder = Notification.Builder(context)
-        builder.setWhen(System.currentTimeMillis())
+        val notification = NotificationCompat.Builder(context, TAG)
+            .setWhen(System.currentTimeMillis())
             .setOngoing(true)
             .setSmallIcon(R.mipmap.ic_launcher)
             .setContentTitle(GlobalAppContext.appName + "保持运行中")
             .setContentText("点击打开【" + GlobalAppContext.appName + "】")
             .setDefaults(NotificationCompat.FLAG_ONGOING_EVENT)
-            .setPriority(Notification.PRIORITY_MAX)
-        //SDK版本>=21才能设置悬挂式通知栏
-        builder.setCategory(Notification.FLAG_ONGOING_EVENT.toString())
-            .setVisibility(Notification.VISIBILITY_PUBLIC)
-        val intent = Intent(context, SplashActivity::class.java)
-        val pi =
-            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-        builder.setContentIntent(pi)
-        manager.notify(null, 0, builder.build())
+            .setPriority(
+                if (VERSION.SDK_INT >= Build.VERSION_CODES.O) NotificationManager.IMPORTANCE_HIGH
+                else NotificationCompat.PRIORITY_MAX
+            )
+            .setCategory(Notification.FLAG_ONGOING_EVENT.toString())
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setContentIntent(pi)
+            .build()
+        manager.notify(null, 0, notification)
     }
 
 }
