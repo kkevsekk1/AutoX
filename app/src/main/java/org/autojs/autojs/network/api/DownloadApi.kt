3e6384634cb2ffbe84d820ada5e07cf623de2a71
sock_magic <startxingcheng@gmail.com>
2024-05-16 10:22:08 +0800

    refactor: 转为 kotlin 文件

--- app/src/main/java/org/autojs/autojs/network/api/DownloadApi.kt
+++ app/src/main/java/org/autojs/autojs/network/api/DownloadApi.kt
@@ -1,19 +1,16 @@
-package org.autojs.autojs.network.api;
+package org.autojs.autojs.network.api
 
-import io.reactivex.Observable;
-import okhttp3.ResponseBody;
-import retrofit2.http.GET;
-import retrofit2.http.Streaming;
-import retrofit2.http.Url;
+import io.reactivex.Observable
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Streaming
+import retrofit2.http.Url
 
 /**
  * Created by Stardust on 2017/12/6.
  */
-
-public interface DownloadApi {
-
+interface DownloadApi {
     @GET
     @Streaming
-    Observable<ResponseBody> download(@Url String url);
-
+    fun download(@Url url: String): Observable<ResponseBody>
 }
