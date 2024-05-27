f50ebb1864a30433a9884683e86284f6d63a0fdd
sock_magic <startxingcheng@gmail.com>
2024-05-27 16:54:44 +0800

    feat: kotlin2.0

--- build-logic/convention/src/main/kotlin/AndroidApplicationComposeConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationComposeConventionPlugin.kt
@@ -2,11 +2,14 @@ import com.android.build.api.dsl.ApplicationExtension
 import org.autojs.autojs.configureAndroidCompose
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.getByType
+
 class AndroidApplicationComposeConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
-            pluginManager.apply("com.android.application")
+            apply(plugin = "com.android.application")
+            apply(plugin = "org.jetbrains.kotlin.plugin.compose")
 
             val extension = extensions.getByType<ApplicationExtension>()
             configureAndroidCompose(extension)
--- build.gradle.kts
+++ build.gradle.kts
@@ -18,5 +18,6 @@ plugins{
     alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.compose) apply false
 }
 
