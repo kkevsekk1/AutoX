5b3bf26fff1e35d28a3842f80c61d9e173ab3a71
aiselp <aiselpk@gmail.com>
2024-07-05 22:31:06 +0800

    移除不可用的maven源

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,6 @@ buildscript {
         google()
         mavenCentral()
         maven("https://www.jitpack.io")
-        maven("https://120.25.164.233:8081/nexus/content/groups/public/")
         maven("https://maven.aliyun.com/repository/central")
         google { url = uri("https://maven.aliyun.com/repository/google") }
         mavenCentral { url = uri("https://maven.aliyun.com/repository/public") }
@@ -37,19 +36,10 @@ allprojects {
         google()
         mavenCentral()
         maven("https://www.jitpack.io")
-        maven("https://120.25.164.233:8081/nexus/content/groups/public/")
         maven("https://maven.aliyun.com/repository/central")
         google { url = uri("https://maven.aliyun.com/repository/google") }
         mavenCentral { url = uri("https://maven.aliyun.com/repository/public") }
     }
-//    tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java){
-//        kotlinOptions{
-//            freeCompilerArgs = freeCompilerArgs.toMutableList().apply {
-//                add("-P")
-//                add("plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true")
-//            }
-//        }
-//    }
 }
 
 tasks.register<Delete>("clean").configure {
