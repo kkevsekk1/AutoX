a4dd06c41d52e9366b78850bb98b62b06879e476
aiselp <aiselpk@gmail.com>
2024-07-09 22:14:27 +0800

    添加v2本地文档

--- app/src/main/java/org/autojs/autojs/ui/main/web/DocumentSource.kt
+++ app/src/main/java/org/autojs/autojs/ui/main/web/DocumentSource.kt
@@ -1,7 +1,12 @@
 package org.autojs.autojs.ui.main.web
 
-enum class DocumentSource(val sourceName: String, val uri: String, val isLocal: Boolean = false) {
-    DOC_V1("在线文档", "http://doc.autoxjs.com/"),
-    DOC_V1_LOCAL("本地文档", "docs/v1", true)
-
+enum class DocumentSource(
+    val sourceName: String,
+    val uri: String,
+    val isLocal: Boolean = false,
+    val openUri: String? = null
+) {
+    DOC_V1("在线文档v1", "http://doc.autoxjs.com/"),
+    DOC_V1_LOCAL("本地文档v1", "docs/v1", true, DOC_V1.uri),
+    DOC_V2_LOCAL("本地文档v2", "docs/v2", true)
 }
